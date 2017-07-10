54ac33869bf6eede0c246e113da24d143120600f
Ben Scholom <bscholom@gmail.com>
2017-07-10 14:09:16 +0300

    created new branch for matrix

--- JS/minecraft.js
+++ JS/minecraft.js
@@ -1,6 +1,15 @@
 //namespace to rule them all
+
 var Minecraft = {};
 
+//create the board
 Minecraft.createMatrix = function() {
-
+	// create game array
+	Minecraft.grid = new array (20);
+	for (var i = 0 ; i < Minecraft.grid.length; i++){
+		Minecraft.grid[i] = new array (20);
+		for( var j = 0; j < Minecraft.grid.length; j++) {
+			Minecraft.grid[]
+		}
+	}
 }
