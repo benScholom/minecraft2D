75be04ce0ac1d3a186103e57d35bbcb248b56664
Deborah <Deborah.Frija@ehl.ch>
2017-07-11 16:51:41 +0300

    Tools clicks

---
+++ JS/features.js
@@ -0,0 +1,11 @@
+$("#shovel").addEventListener("click",shovel)
+        function shovel(event){
+            $("#shovel").style.border = "3px solid rgb(129, 14, 5)";
+            $("#shovel").style.borderRadius = "15px";
+        }
+
+$("#pickaxe").addEventListener("click",pickaxe)
+        function pickaxe(event){
+            $("#pickaxe").style.border = "3px solid rgb(129, 14, 5)";
+            $("#pickaxe").style.borderRadius = "15px";
+        }
